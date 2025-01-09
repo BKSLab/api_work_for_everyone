273b91dabd90e89c0b3f00544fcb00116ab9e9f4
BKSLab <bks2408@mail.ru>
2025-01-09 22:18:19 +0400

    Создал базовую структуру приложения
    Создал базовую структуру приложения, добавил Dockerfile и файд
    docker-compose.yml для запуска проекта в Docker контейнерах.

---
+++ api_service_main.py
@@ -0,0 +1,32 @@
+from http import HTTPStatus
+
+import uvicorn
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+
+app = FastAPI()
+
+
+@app.get(path='/api/regions', tags=['Список регионов'])
+async def get_regions_list():
+   """Обрабатывает GET запрос на получение списка регионов."""
+   return JSONResponse(
+        content={
+           'id': 1,
+           'region_name': 'Республика Адыгея (Адыгея)',
+           'region_code_tv': '01',
+           'region_code_hh': '1422'
+        },
+        status_code=HTTPStatus.OK
+    )
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        app='api_service_main:app',
+        reload=True,
+        host='0.0.0.0',
+        port=8000
+    )
