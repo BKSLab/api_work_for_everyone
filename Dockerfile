d61af6309fe7af163dc8c4c9621c0075397222b0
BKSLab <bks2408@mail.ru>
2025-02-24 23:38:03 +0400

    Подготовил репозиторий для работы над проектом.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
 FROM python:3.12-slim
 
+ENV PYTHONUNBUFFERED=1
+ENV PYTHONDONTWRITEBYTECODE=1
+
 WORKDIR /app
+
+RUN apt-get update \
+    && apt-get -y install libpq-dev gcc
 
 COPY . /app
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,7 @@
-python main.py
+# Указываем путь к src, чтобы Python правильно искал модули
+export PYTHONPATH=/app/src
+
+# Создаем БД перед запуском приложения
+python -m src.db.create_db
+
+python -m src.main
